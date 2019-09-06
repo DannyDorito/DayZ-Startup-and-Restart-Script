3b789c2f4856522fbb966ae0f0fa393f7ad2d3c4
Niklas Henrixon <nixon@skyn1.se>
2019-09-06 14:36:52 +0200

    Added profiles support
    Done some cleaning and tidy up the code a bit. Also added support for profiles

--- DayZServerStart.bat
+++ DayZServerStart.bat
@@ -1,86 +1,104 @@
 ::
 :: DayZServerStart.bat
 :: By: Jstrow and Danny Dorito, originally for CSG Exile
+:: Minor edits by NIXON : https://github.com/niklashenrixon
 ::
-@echo off
-color F
-echo Pre startup initialised
-echo.
+@ECHO off
+COLOR F
+
 :: Command window name, does not affect anything else
 :: Default is: DayZ Server
-set server_name=DayZ Server
+SET S_NAME=DayZ Server
+TITLE %S_NAME%
 
-:: Path to the DayZ server executable, for example C:DayZServer\
-set path_to_server_executable=changeme
+ECHO Pre startup initialised
+
+:: Path to the DayZ server executable, for example:  C:\Program Files (x86)\Steam\steamapps\common\DayZServer
+SET EXE_PATH=changeme
+
 :: Name of executable
 :: Default is: DayZServer_x64.exe
-set exe_name=DayZServer_x64.exe
+SET EXE=DayZServer_x64.exe
+
 :: Extra launch parameters
 :: For more info see: https://forums.dayz.com/topic/239635-dayz-server-files-documentation/?tab=comments#comment-2396561
-set extra_launch_parameters="-dologs -adminlog -netlog -freezecheck"
-:: set the port number of the DayZ server, default ARMA is 2303
-set server_port_number=0
-:: set the DayZ config file, default is serverDZ.cfg
-set config_name=serverDZ.cfg
-::
-:: DO NOT CHANGE ANYTHING BELOW THIS POINT
-:: UNLESS YOU KNOW WHAT YOU ARE DOING
-::
-set error=""
+SET PARAMETERS=-doLogs -adminLog -netLog -freezeCheck
 
-echo.
-echo Starting vars checks
-title %server_name%
+:: Set the port number of the DayZ server, default is 2302
+SET PORT=0
 
-if "%path_to_server_executable%" == "changeme" (
-	set error=path_to_server_executable
-	goto error
-)
-if "%server_port_number%" == "0" (
-	set error=server_port_number
-	goto error
-)
-set tasklist_name=IMAGENAME eq %exe_name%
+:: Set the DayZ config file, default is serverDZ.cfg
+SET CONFIG=serverDZ.cfg
 
-echo.
-echo Variable checks completed!
-echo.
-set loops=0
+:: Profile name, e.g: MyFirstDayzServer
+SET PROFILE=changeme
 
-:loop
-tasklist /FI "%tasklist_name%" 2>NUL | find /I /N "%server_port_number%">NUL
-if "%ERRORLEVEL%" == "0" goto loop
 
-echo.
-echo Pre startup complete!
-echo.
-echo Starting server at: %date%,%time%
-if "%loops%" NEQ "0" (
-	echo Restarts: %loops%
+:: ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: ::
+::             DO NOT CHANGE ANYTHING BELOW THIS POINT               ::
+::               UNLESS YOU KNOW WHAT YOU ARE DOING                  ::
+:: ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: ::
+
+
+SET ERROR=""
+
+ECHO.
+ECHO Starting vars checks
+
+IF "%EXE_PATH%" == "changeme" (
+	SET ERROR=EXE_PATH
+	GOTO ERROR
+	)
+
+IF "%PORT%" == "0" (
+	SET ERROR=PORT
+	GOTO ERROR
+	)
+
+IF "%PROFILE%" == "changeme" (
+	SET ERROR=PROFILE
+	GOTO ERROR
+	)
+
+SET T_NAME=IMAGENAME eq %EXE%
+
+ECHO.
+ECHO Variable checks completed!
+SET LOOPS=0
+
+:LOOP
+TASKLIST /FI "%T_NAME%" 2>NUL | find /I /N "%PORT%">NUL
+IF "%ERRORLEVEL%" == "0" GOTO LOOP
+
+ECHO.
+ECHO Pre startup complete!
+ECHO Starting server at: %DATE%,%TIME%
+IF "%LOOPS%" NEQ "0" (
+	ECHO Restarts: %LOOPS%
 )
 
 :: Start the DayZ Server
-cd %path_to_server_executable%
-start "%server_name%" /min /wait %exe_name% -config=%config_name% -port=%server_port_number%  %extra_launch_parameters%
-echo To stop the server, close %~nx0 then the other tasks, otherwise it will restart
-echo.
-goto looping
+CD %EXE_PATH%
+START "%S_NAME%" /wait %EXE% -config=%CONFIG% -profiles=%PROFILE% -port=%PORT% %PARAMETERS%
+ECHO To stop the server, close %~nx0 then the other tasks, otherwise it will restart
+ECHO.
+GOTO LOOPING
 
-:loop
 :: Monitoring Loop
-echo Server is already running, running monitoring loop
+:LOOP
+ECHO Server is already running, running monitoring loop
 
-:looping
 :: Restart/Crash Handler
-set /A loops+=1
-timeout /t 5
-tasklist /FI "%tasklist_name%" 2>NUL | find /I /N "%server_port_number%">NUL
-if "%ERRORLEVEL%"=="0" goto loop
-goto loop
+:LOOPING
+SET /A LOOPS+=1
+TIMEOUT /t 5
+TASKLIST /FI "%T_NAME%" 2>NUL | find /I /N "%PORT%">NUL
+IF "%ERRORLEVEL%"=="0" GOTO loop
+GOTO loop
 
-:error
 :: Generic error catching
-color C
-echo ERROR: %error% not set correctly, please check the config
-pause
-color F
+:ERROR
+COLOR C
+ECHO ERROR: %ERROR% not set correctly, please check the config
+PAUSE
+COLOR F

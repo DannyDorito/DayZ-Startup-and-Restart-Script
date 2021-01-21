948a7fec5aa33def4ac7c0d281a6bbd34a95c940
Danny Dorito <32311622+DannyDorito@users.noreply.github.com>
2021-01-21 23:16:17 +0000

    Added Optional Database Backup

--- DayZServerStart.bat
+++ DayZServerStart.bat
@@ -10,16 +10,13 @@ ECHO MESSAGE: Pre startup initialised
 :: Command window name, does not affect anything else
 :: Default is: DayZ Server
 SET S_NAME=DayZ Server
-
 :: Path to the DayZ server executable, for example:  C:\Program Files (x86)\Steam\steamapps\common\DayZServer or C:\dayzserver
 :: Cannot be blank or 'changeme'
 SET EXE_PATH=changeme
-
 :: Name of executable
 :: Default is: DayZServer_x64.exe
 :: Cannot be blank
 SET EXE=DayZServer_x64.exe
-
 :: List of client side mods, Add the mod to modlist for example adding Mod3 to set modlist=@Mod1; @Mod2;
 :: You would do: set modlist=@Mod1; @Mod2; @Mod3
 :: Cannot be '@Mod1; @Mod2; @Mod3;'
@@ -27,33 +24,28 @@ set MODLIST=@Mod1; @Mod2; @Mod3;
 :: List of server side mods, Add the mod to servermodlist for example adding ServerMod3 to set servermodlist=@ServerMod1; @ServerMod2;
 :: You would do: set SERVERMODLIST=@ServerMod1; @ServerMod2; @ServerMod3;
 set SERVERMODLIST=
+:: Set the port number of the DayZ server, default is 2302
+:: Cannot be blank or 0
+SET PORT=2302
+:: Set the DayZ config file, default is serverDZ.cfg
+:: Cannot be blank
+SET CONFIG=serverDZ.cfg
+:: Profile name, e.g: MyFirstDayzServer
+:: Cannot be blank or 'changeme'
+SET PROFILE=changeme
 
 :: Extra launch parameters
 :: For more info see: https://forums.dayz.com/topic/239635-dayz-server-files-documentation/?tab=comments#comment-2396561
 SET ADDITIONAL_PARAMETERS=-doLogs -adminLog -netLog -freezeCheck
-
-:: Set the port number of the DayZ server, default is 2302
-:: Cannot be blank or 0
-SET PORT=2302
-
-:: Set the DayZ config file, default is serverDZ.cfg
-:: Cannot be blank
-SET CONFIG=serverDZ.cfg
-
-:: Profile name, e.g: MyFirstDayzServer
-:: Cannot be blank or 'changeme'
-SET PROFILE=changeme
 
 :: Enables the DayZ SA Launcher to download mods running on the server,
 :: For more info see: https://dayzsalauncher.com/#/tools
 :: Set to true to enable, false to disable
 :: Default is: false
 SET USE_DZSAL_MODSERVER=false
-
 :: Name of DayZ SA Launcher Mod Server exe
 :: Default is: DZSALModServer.exe
 SET EXE_DZSAL=DZSALModServer.exe
-
 :: Extra launch parameters
 :: For more info see command line parameters section of: https://dayzsalauncher.com/#/tools
 SET DZSAL_PARAMETERS=changeme
@@ -63,22 +55,30 @@ SET DZSAL_PARAMETERS=changeme
 :: Set to true to enable, false to disable
 :: Default is: false
 SET %USE_STEAM_UPDATER%=false
-
 :: Path to the DayZ server executable, for example:  C:\Program Files (x86)\SteamCMD
 SET %PATH_TO_STEAM_CMD_EXE%=changeme
-
 :: Name of the Steam account that SteamCMD uses
 :: It is highly advised that you use a separate Steam account for the DayZ server if you choose to use this feature
 :: 2FA may be an issues always please be careful with passwords
 SET %ACCOUNT_NAME%=changeme
-
 :: It is highly advised that you use a separate Steam account for the DayZ server
 :: Password of the Steam account that SteamCMD uses, 2FA may be an issues always please be careful with passwords
 SET %ACCOUNT_PASSWORD%=changeme
-
 :: Addition apps or mods that you wish SteamCMD to update for you, this will have to match the workshop item id, for example 2288339650 2288336145 for Namalsk
 SET %ADDITIONAL_ITEMS%=
 
+:: If you are using the SQL backup set backup=true
+:: Default is false
+set BACKUP=false
+:: set the directory to the .bat filePatching, for example C:ARMA\backup.bat
+set PATH_TO_SQL_BACKUP=changeme
+:: if you want the script to move the created SQL backup to another directory, set move_backup=true
+:: Default is false
+set MOVE_BACKUP=false
+:: directory to move the files FROM, for example C:ARMA/backup
+set Backup=changeme
+:: directory to move files TO, for example C:/dropbox
+set BACKUP_TO=changeme
 
 :: ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: ::
 ::             DO NOT CHANGE ANYTHING BELOW THIS POINT               ::
@@ -96,7 +96,6 @@ IF "%EXE_PATH%" == "changeme" (
 	SET ERROR=EXE_PATH
 	GOTO ERROR
 )
-
 IF [%EXE_PATH%] == [] (
 	SET ERROR=EXE_PATH
 	GOTO ERROR
@@ -116,7 +115,6 @@ IF %PORT% == 0 (
 	SET ERROR=PORT
 	GOTO ERROR
 )
-
 IF [%PORT%] == [] (
 	SET ERROR=PORT
 	GOTO ERROR
@@ -126,7 +124,6 @@ IF "%PROFILE%" == "changeme" (
 	SET ERROR=PROFILE
 	GOTO ERROR
 )
-
 IF [%PROFILE%] == [] (
 	SET ERROR=PROFILE
 	GOTO ERROR
@@ -175,18 +172,40 @@ IF "%USE_STEAM_UPDATER%" == "true" (
 	)
 )
 
+IF "%BACKUP%" == "true"(
+	IF "%MOVE_BACKUP%" == "true" (
+		IF "%BACKUP_FROM%" == "changeme" (
+			SET ERROR=BACKUP_FROM
+			GOTO ERROR
+		)
+		IF [%BACKUP_FROM%] == [] (
+			SET ERROR=MOVE_BACKUP = true so, BACKUP_FROM
+			GOTO ERROR
+		)
+		IF "%BACKUP_TO%" == "changeme" (
+			SET ERROR=BACKUP_TO
+			GOTO BACKUP_TO
+		)
+		IF [%ACCOUNT_PASSWORD%] == [] (
+			SET ERROR=MOVE_BACKUP = true so, ACCOUNT_PASSWORD
+			GOTO ERROR
+		)
+	)
+	IF "%PATH_TO_SQL_BACKUP%" == "changeme" (
+		SET ERROR=PATH_TO_SQL_BACKUP
+		GOTO PATH_TO_SQL_BACKUP
+	)
+	IF [%PATH_TO_SQL_BACKUP%] == [] (
+		SET ERROR=BACKUP = true so, PATH_TO_SQL_BACKUP
+		GOTO ERROR
+	)
+)
+
 SET T_NAME=IMAGENAME eq %EXE%
 
 ECHO.
 ECHO MESSAGE: Variable checks completed!
 SET LOOPS=0
-
-IF "%USE_STEAM_UPDATER%" == "true" (
-	ECHO MESSAGE: Steam Automatic Update Starting
-	CD %PATH_TO_STEAM_CMD_EXE%
-	START /wait "SteamCMD.exe" +login %ACCOUNT_NAME% %ACCOUNT_PASSWORD% +force_install_dir %EXE_PATH% +app_update 223350 %ADDITIONAL_ITEMS% validate +quit
-	ECHO MESSAGE: Steam Automatic Update Completed
-)
 
 :LOOP
 TASKLIST /FI "%T_NAME%" 2>NUL | find /I /N "%PORT%">NUL
@@ -197,6 +216,23 @@ ECHO MESSAGE: Pre startup complete!
 ECHO MESSAGE: Starting server at: %DATE%, %TIME%
 IF "%LOOPS%" NEQ "0" (
 	ECHO MESSAGE: Restarts: %LOOPS%
+)
+
+:: Uses https://www.redolive.com/utah-web-designers-blog/automated-mysql-backup-for-windows
+IF "%BACKUP%" == "true" (
+	ECHO MESSAGE: Starting Database Backup
+	START /wait %PATH_TO_SQL_BACKUP%
+	IF "%MOVE_BACKUP%" == "true" (
+		MOVE /wait /-Y  "%BACKUP_FROM%"*.* "%BACKUP_TO%"
+	)
+	ECHO MESSAGE: Database backup complete
+)
+
+IF "%USE_STEAM_UPDATER%" == "true" (
+	ECHO MESSAGE: Steam Automatic Update Starting
+	CD %PATH_TO_STEAM_CMD_EXE%
+	START /wait "SteamCMD.exe" +login %ACCOUNT_NAME% %ACCOUNT_PASSWORD% +force_install_dir %EXE_PATH% +app_update 223350 %ADDITIONAL_ITEMS% validate +quit
+	ECHO MESSAGE: Steam Automatic Update Completed
 )
 
 :: Start the DayZ Server
